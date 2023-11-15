d87079cfb0eed23ce32d4cccd57fc5c959241a79
melchongx <melfredfonclara@gmail.com>
2023-11-15 20:35:37 +0800

    Add project files.

---
+++ IT-ELEC1C_3ITE_ GRP6/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT_ELEC1C_3ITE__GRP6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ IT-ELEC1C_3ITE_ GRP6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT_ELEC1C_3ITE__GRP6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
