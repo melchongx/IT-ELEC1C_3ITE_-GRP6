c4d101780dc33808b5b98cfdc0118522262814dd
melchongx <melfredfonclara@gmail.com>
2023-11-20 09:52:05 +0800

    models b

---
+++ IT-ELEC1C_3ITE_ GRP6/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace IT_ELEC1C_3ITE__GRP6.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string userFname { get; set; }
+        public string userLname { get; set; }
+        public string userUname { get; set; }
+        public string userPwd { get; set; }
+        public string userEmail { get; set; }
+        public DateTime userBday { get; set; }
+    }
+}
