d8fabdda54d3ded2b6360a921cedec80c02044f4
Reyko2 <rico.nieto.cics@ust.edu.ph>
2023-12-04 03:23:41 +0800

    New models for innonet

--- IT-ELEC1C_3ITE_ GRP6/Startup.cs
+++ IT-ELEC1C_3ITE_ GRP6/Startup.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using IT_ELEC1C_3ITE__GRP6.Data;
 using IT_ELEC1C_3ITE__GRP6.Models;
 using IT_ELEC1C_3ITE__GRP6.Services;
 using Microsoft.AspNetCore.Mvc;
+using InnoNet.Data;
 
 namespace IT_ELEC1C_3ITE__GRP6
 {
---
+++ InnoNet.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace InnoNet.Data
+{
+    public class Class1
+    {
+
+    }
+}
