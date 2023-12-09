5c11f975ae8ba33c3a6f86880f0a6a53fbb4e456
melchongx <melfredfonclara@gmail.com>
2023-12-10 01:50:19 +0800

    s

--- IT-ELEC1C_3ITE_ GRP6/Controllers/HomeController.cs
+++ IT-ELEC1C_3ITE_ GRP6/Controllers/HomeController.cs
@@ -13,14 +13,12 @@ namespace IT_ELEC1C_3ITE__GRP6.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Clubs()
         {
-            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
