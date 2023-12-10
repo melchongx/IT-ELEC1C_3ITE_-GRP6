551a68d090626cda210e87ec619157ac4bea43d3
Reyko2 <124423979+Reyko2@users.noreply.github.com>
2023-12-10 22:14:26 +0800

    Delete IT-ELEC1C_3ITE_ GRP6 directory

--- IT-ELEC1C_3ITE_ GRP6/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IT_ELEC1C_3ITE__GRP6.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
